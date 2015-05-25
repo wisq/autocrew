da1ff9277635d399787ecb7ab07d6bd2bef77cf3
Adrian Irving-Beer <wisq@wisq.net>
2015-05-25 03:32:18 -0400

    Add ObjectRegistry tests.

--- lib/glomp/object_registry.rb
+++ lib/glomp/object_registry.rb
@@ -2,6 +2,9 @@ require 'json'
 
 module Glomp
   class ObjectRegistry
+    class RegisterMismatchError < StandardError; end
+    class RegisterCollisionError < StandardError; end
+
     def initialize
       @by_id = {}
       @by_object = {}
@@ -11,20 +14,22 @@ module Glomp
     def register(object, id = nil)
       if old_id = @by_object[object]
         return if id == nil || id == old_id
-        raise "Attempt to re-register object #{object.inspect} with new ID (#{old_id} vs #{id})"
+        raise RegisterMismatchError.new("Attempt to re-register object #{object.inspect} with new ID (#{old_id} vs #{id})")
       end
 
       id ||= (@next_id += 1)
+
+      if old_object = @by_id[id]
+        return if old_object.eql?(object)
+        raise RegisterCollisionError.new("ID #{id} already taken by #{old_object.inspect}, cannot assign to #{object.inspect}")
+      end
+
       @by_id[id] = object
       @by_object[object] = id
     end
 
     def lookup(id)
       @by_id[id]
-    end
-
-    def registered?(object)
-      @by_object.has_key?(object)
     end
   end
 end
---
+++ test/glomp/object_registry_test.rb
@@ -0,0 +1,48 @@
+require 'minitest_helper'
+require 'glomp/object_registry'
+
+module Glomp
+  class ObjectRegistryTest < Minitest::Test
+    def setup
+      @registry = ObjectRegistry.new
+    end
+
+    test "register and lookup" do
+      assert_equal 1, @registry.register(obj1 = mock)
+      assert_equal 2, @registry.register(obj2 = mock)
+      assert_equal 3, @registry.register(obj3 = mock)
+      assert_equal obj1, @registry.lookup(1)
+      assert_equal obj2, @registry.lookup(2)
+      assert_equal obj3, @registry.lookup(3)
+    end
+
+    test "register with explicit ID" do
+      assert_equal 4, @registry.register(obj1 = mock, 4)
+      assert_equal 5, @registry.register(obj2 = mock, 5)
+      assert_equal 6, @registry.register(obj3 = mock, 6)
+      assert_equal obj1, @registry.lookup(4)
+      assert_equal obj2, @registry.lookup(5)
+      assert_equal obj3, @registry.lookup(6)
+    end
+
+    test "explicit/implicit ID collision" do
+      assert_equal 1, @registry.register(mock, 1)
+
+      assert_raises ObjectRegistry::RegisterCollisionError do
+        @registry.register(mock, 1)
+      end
+
+      assert_raises ObjectRegistry::RegisterCollisionError do
+        @registry.register(mock)
+      end
+    end
+
+    test "multiple register attempt" do
+      obj = mock
+      assert_equal 1, @registry.register(obj)
+      assert_raises ObjectRegistry::RegisterMismatchError do
+        @registry.register(obj, 2)
+      end
+    end
+  end
+end
